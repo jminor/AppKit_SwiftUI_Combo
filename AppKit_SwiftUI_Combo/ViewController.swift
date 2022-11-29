e77b2b4b9203e844eef54fc1ba6d5d4dcc31feac
Joshua Minor <jminor@users.noreply.github.com>
2022-11-28 22:22:35 -0800

    Initial Commit

---
+++ AppKit_SwiftUI_Combo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AppKit_SwiftUI_Combo
+//
+//  Created by Joshua Minor on 11/28/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
